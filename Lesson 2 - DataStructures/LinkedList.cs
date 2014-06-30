dc205fc66ba2931f993cd9b12ebb3748583aed11
Zachary Yates <zachary.yates@gmail.com>
2014-06-30 11:10:33 -0700

    added lesson 2

---
+++ Lesson 2 - DataStructures/LinkedList.cs
@@ -0,0 +1,45 @@
+﻿
+namespace Lesson2DataStructures {
+    class ListNode<T> {
+        public ListNode(T data) {
+            Data = data;
+        }
+
+        public T Data;
+        public ListNode<T> Next;
+        public ListNode<T> Prev;
+
+        public override string ToString() {
+            return Data != null
+                ? Data.ToString()
+                : null;
+        }
+    }
+
+    class LinkedList<T> {
+        ListNode<T> _first = null;
+        ListNode<T> _last = null;
+
+        public void Add(T item) {
+            var node = new ListNode<T>(item);
+            if (_first == null) { 
+                // first node in the list, no other nodes to link to
+                _first = node;
+                _last = node;
+            } else { 
+                // for all other nodes in the list, we have to link them up to the last node
+                _last.Next = node;
+                node.Prev = _last;
+                _last = node;
+            }
+        }
+
+        public ListNode<T> GetLast() {
+            return _last;
+        }
+
+        public ListNode<T> GetFirst() {
+            return _first;
+        }
+    }
+}
---
+++ Lesson 2 - DataStructures/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace Lesson2DataStructures {
+    class Program {
+        static void Main(string[] args) {
+            var list = new LinkedList<Deck>();
+            list.Add(new Deck(1, "Control Deck"));
+            list.Add(new Deck(2, "Sweet Burn Deck"));
+            list.Add(new Deck(3, "White Weenie"));
+            list.Add(new Deck(4, "Green Stompy"));
+            list.Add(new Deck(5, "Ernam Burn'em"));
+            list.Add(new Deck(6, "Another"));
+            
+            var currentNode = list.GetFirst();
+            while (true) {
+                Console.WriteLine(currentNode);
+                if (currentNode.Next == null)
+                    break;
+                currentNode = currentNode.Next; // moves to the next item in the list
+            }
+
+            currentNode = list.GetLast();
+            while (true) {
+                Console.WriteLine(currentNode);
+                if (currentNode.Prev == null)
+                    break;
+                currentNode = currentNode.Prev; // moves to the prev item in the list
+            }
+
+            Console.Read();
+        }
+    }
+
+    class Deck {
+        public string Title;
+        public int Id;
+
+        public Deck(int id, string title) {
+            Id = id;
+            Title = title;
+        }
+
+        public override string ToString() {
+            return string.Format("{0}: {1}", Id, Title);
+        }
+    }
+
+    class Card {
+        
+    }
+}
